edcd638e290019f002fe3452461ea5091c3ebcbf
iHakan <ihakan@yaani.com>
2020-07-16 00:35:28 +0300

    Buton fonksiyonları eklendi sayfa yönlendirmelerine başlandı

--- arayuzBir/App.js
+++ arayuzBir/App.js
@@ -11,35 +11,74 @@ import {
 
 const renk1 = '#ff003f';
 const renk2 = '#303463';
-
+let sayfa = '';
 const W = Dimensions.get('window').width;
 const H = Dimensions.get('window').height;
 
 class Arayuz1 extends React.Component {
+  signUpOnPress() {
+    alert('Test Başarılı!!!');
+  }
+
+  signInOnPress() {
+    alert('SignIn Test Başarılı!!!');
+  }
+
+  signUp() {
+    return <Text>signUp</Text>;
+  }
+
+  signIn() {
+    return <Text>signIn</Text>;
+  }
+
+  butonlar() {
+    return (
+      <View style={stil.buttonContainer}>
+        <TouchableOpacity
+          activeOpacity={0.4}
+          style={[stil.signUpButton, stil.button]}
+          onPress={() => {
+            this.signUpOnPress();
+          }}>
+          <Text style={stil.signUpButtonText}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.4}
+          style={[stil.signInButton, stil.button]} onPress={() => {
+            this.signInOnPress();
+          }}>
+          <Text
+            style={stil.signInButtonText}
+            >
+            Sign In
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={stil.bodyContainer}>
-
-        <StatusBar translucent barStyle={'light-content'} backgroundColor={renk2}/>
+        <StatusBar
+          translucent
+          barStyle={'light-content'}
+          backgroundColor={renk2}
+        />
 
         <View style={stil.topContainer}>
-          <Image style={stil.logo} source={require('./assets/images/logo1.png')} />
+          <Image
+            style={stil.logo}
+            source={require('./assets/images/logo1.png')}
+          />
         </View>
 
         <View style={stil.bottomContainer}>
-          <View style={stil.buttonContainer}>
-            <TouchableOpacity
-              activeOpacity
-              style={[stil.signUpButton, stil.button]}>
-              <Text style={stil.signUpButtonText}>Sign Up</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              activeOpacity ={0.4}
-              style={[stil.signInButton, stil.button]}>
-              <Text style={stil.signInButtonText}>Sign In</Text>
-            </TouchableOpacity>
-          </View>
+          {sayfa === '' && this.butonlar()}
+          {sayfa === 'signUp' && this.signUp()}
+          {sayfa === 'ssignIn' && this.signIn()}
         </View>
       </View>
     );
@@ -53,23 +92,24 @@ const stil = StyleSheet.create({
   },
   topContainer: {
     flex: 1,
- 
+
     justifyContent: 'center',
-    alignItems:'center'
+    alignItems: 'center',
   },
+
   logo: {
-    width: W * 0.58,
-    height: H * 0.3,
+    width: W * 0.54,
+    height: H * 0.28,
   },
-  
+
   buttonContainer: {
-    alignItems:'center',
+    alignItems: 'center',
     height: H * 0.25,
   },
   button: {
     width: W * 0.7,
     height: H * 0.05,
-    marginBottom: H*0.01,
+    marginBottom: H * 0.01,
     paddingTop: 5,
     paddingBottom: 5,
     justifyContent: 'center',
